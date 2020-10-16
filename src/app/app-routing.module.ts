bb3a73468b1a9f8bdb704149bb50dbcba900ead5
Jesus Meza <jemezor07@hotmail.com>
2020-10-16 13:23:07 -0500

    MV1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearcherComponent } from './components/searcher/searcher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '**',
+    component: SearcherComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearcherComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
